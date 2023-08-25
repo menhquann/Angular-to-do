cdb69f76b95d188555e064d3ad508dfe604bfd3f
Quan Nguyen <menhquann@gmail.com>
2023-08-25 17:17:10 +0700

    feat: CRUD task

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Task } from 'src/app/Task';
+
+@Component({
+    selector: 'app-add-task',
+    templateUrl: './add-task.component.html',
+    styleUrls: ['./add-task.component.scss'],
+})
+export class AddTaskComponent {
+    @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+    text!: string;
+    day!: string;
+    reminder: boolean = false;
+
+    onSubmit() {
+        if (!this.text) {
+            alert('Please add a task');
+            return;
+        }
+        const newTask = {
+            text: this.text,
+            day: this.day,
+            reminder: this.reminder,
+        };
+        this.onAddTask.emit(newTask);
+        this.text = '';
+        this.day = '';
+        this.reminder = false;
+    }
+}
